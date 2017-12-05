2ab058dadd216b71ed2d9f403bdb00a14a529b8a
BaeInpyo <dkas2@snu.ac.kr>
2017-12-05 21:44:49 +0900

    reward function basic implementation

--- reward_function.py
+++ reward_function.py
@@ -1,5 +1,15 @@
+import numpy as np
+
 def calcReward(obs):
     ## write your own reward
-    reward=0
-    return reward
+    track = np.array(obs['track'])
+    trackPos = np.array(obs['trackPos'])
+    sp = np.array(obs['speedX'])
+    damage = np.array(obs['damage'])
+    rpm = np.array(obs['rpm'])
 
+    progress = sp*np.cos(obs['angle']) - np.abs(sp*np.sin(obs['angle'])) - \
+            sp*np.abs(obs['trackPos'])
+    reward = progress
+
+    return reward
