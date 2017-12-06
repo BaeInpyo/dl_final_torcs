4e1349c757736b7d59fc82e52d93de1979c0fa67
jeonghyun <jh800222@gmail.com>
2017-12-06 14:40:54 +0000

    bias: constant-> variable

--- CriticNetwork.py
+++ CriticNetwork.py
@@ -52,26 +52,38 @@ class CriticNetwork(object):
             input_state = tf.placeholder(tf.float32, shape=[None, state_size])
             ## will come from action network
             input_action = tf.placeholder(tf.float32, shape=[None, action_dim])
-
+            """
             wf1 = tf.get_variable(name='wf1', shape=[state_size, HIDDEN1_UNIT])
             wf2_s = tf.get_variable(name='wf2_s', shape=[HIDDEN1_UNIT, HIDDEN2_UNIT])
             wf2_a = tf.get_variable(name='wf2_a', shape=[action_dim, HIDDEN2_UNIT])
             wf2 = tf.get_variable(name='wf2', shape=[HIDDEN2_UNIT, HIDDEN2_UNIT])
             wlogits = tf.get_variable(name='wlogits', shape=[HIDDEN2_UNIT, action_dim])
-
+            
             bf1 = tf.constant(0.0, shape=[HIDDEN1_UNIT])
             bf2_s = tf.constant(0.0, shape=[HIDDEN2_UNIT])
             bf2_a = tf.constant(0.0, shape=[HIDDEN2_UNIT])
             bf2 = tf.constant(0.0, shape=[HIDDEN2_UNIT])
             blogits = tf.constant(0.0, shape=[action_dim])
-
+            
             fc1 = tf.nn.relu(tf.add(tf.matmul(input_state, wf1), bf1))
             fc2_s = tf.add(tf.matmul(fc1, wf2_s), bf2_s)
             fc2_a = tf.add(tf.matmul(input_action, wf2_a), bf2_a)
             fc2_sum = tf.add(fc2_s, fc2_a)
             fc2 = tf.nn.relu(tf.add(tf.matmul(fc2_sum, wf2), bf2))
             logits = tf.add(tf.matmul(fc2, wlogits), blogits)
+            """
+            wf1 = self.variable([state_size, HIDDEN1_UNIT], state_size)
+            wf2_s = self.variable([HIDDEN1_UNIT, HIDDEN2_UNIT], HIDDEN1_UNIT+action_dim)
+            wf2_a = self.variable([action_dim, HIDDEN2_UNIT], HIDDEN1_UNIT+action_dim)
+            wf3 = tf.Variable(tf.random_uniform([HIDDEN2_UNIT, 1],-3e-3,3e-3))
+            b1 = self.variable([HIDDEN1_UNIT], state_size)
+            b2 = self.variable([HIDDEN2_UNIT], HIDDEN1_UNIT+action_dim)
+            b3 = tf.Variable(tf.random_uniform([1],-3e-3,3e-3))
 
+            fc1 = tf.nn.relu(tf.matmul(input_state, wf1) + b1)
+            fc2 = tf.nn.relu(tf.matmul(fc1, wf2_s) + tf.matmul(input_action, wf2_a) + b2)
+            logits = tf.identity(tf.matmul(fc2, wf3) + b3)    
+            
             return logits, input_action, input_state
 
  
@@ -80,3 +92,6 @@ class CriticNetwork(object):
 
     def target_predict(self, state_and_action):
         return self.sess.run(self.target_output, feed_dict={self.target_state: state_and_action[0], self.target_action: state_and_action[1] })
+
+    def variable(self,shape,f):
+        return tf.Variable(tf.random_uniform(shape,-1/math.sqrt(f),1/math.sqrt(f)))
